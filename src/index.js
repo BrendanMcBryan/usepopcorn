962d458609d2a31566c5e5ae807f1288781d11bb
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-02-22 15:27:13 -0800

    passing props to independent components

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
 
-    <StarRating maxRating={10}/>
-
+    <StarRating maxRating={3} messages={['bad', 'okay', 'good']} />
+    <StarRating
+      maxRating={17}
+      size={24}
+      color="red"
+      classname="test"
+      defaultRating={8}
+    />
   </React.StrictMode>
 );
-
